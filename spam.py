20073a910f9ac4b65021fbdd14af6f434d2b34d8
Satoru908 <satorugojo908@gmail.com>
2024-07-01 23:07:05 -0500

    Update spam.py

--- spam.py
+++ spam.py
@@ -47,8 +47,8 @@ def reenviar_mensajes(client):
                         if error_str not in errores_impresos:
                             print(f"Error al enviar mensaje al grupo {chat.title}: {error_str}")
                             errores_impresos.add(error_str)
-            print("Esperar 1 minuto para enviar el proximo mensaje.")
-            sleep(60)  # Esperar 1 minuto para enviar el proximo mensaje.
+            print("Esperar 10 segundos para enviar el proximo mensaje.")
+            sleep(10)  # Esperar 10 segundos para enviar el proximo mensaje.
 
     except Exception as ex:
         print(f"Error general: {ex}")
@@ -60,7 +60,7 @@ if __name__ == "__main__":
     while True:
         try:
             reenviar_mensajes(client)
-            print("Esperar 1 minuto para enviar el proximo mensaje.")
-            sleep(60)  # Esperar 1 minuto para enviar el proximo mensaje.
+            print("Esperar 10 segundos para enviar el proximo mensaje.")
+            sleep(10)  # Esperar 10 segundos para enviar el proximo mensaje.
         except Exception as ex:
             print(f"Error general: {ex}")
